66ed77830766e60722ed168de102b4f0e50e2757
Elod Csirmaz <code@epcsirmaz.co.uk>
2025-10-31 01:45:13 +0000

    More info on hulls

--- v2/convexhull.py
+++ v2/convexhull.py
@@ -9,7 +9,7 @@ import numpy as np
 
 sys.path.append(os.path.dirname(__file__) + "/../../openscad-py")  # https://codeberg.org/csirmaz/openscad-py
 
-from openscad_py import Sphere, Collection, Header
+from openscad_py import Sphere, Collection, Header, Polyhedron
 
 small_spheres = []
 big_spheres = []
@@ -40,6 +40,7 @@ def check_necessary_condition(desc):
 
 points = []
 descriptors = []
+nc_points = [] # indices of NC points
 
 for line in sys.stdin:
     match = re.search(r'^Sum: ([0-9]+), ([0-9]+), ([0-9]+) \(([\-0-9]+)\) <([^>]+)>', line)
@@ -49,21 +50,35 @@ for line in sys.stdin:
         z = int(match.group(3)) * zoom
         itr = int(match.group(4))
         desc = match.group(5)
-        points.append([x,y,z])        
-        descriptors.append(f"({itr:2.0f}) <{desc}> {'NC' if check_necessary_condition(desc) else ''}")
+        points.append([x,y,z])
+        is_nc = check_necessary_condition(desc)
+        if is_nc: nc_points.append(len(points)-1)
+        descriptors.append(f"({itr:2.0f}) <{desc}>")
         #small_spheres.append(Sphere(small_r).move([x,y,z]))
         #c = colors[itr]
         #big_spheres.append(Sphere(big_r).move([x,y,z]).color(*c))
 
 points = np.array(points)
-chull = ConvexHull(points)
+chull = ConvexHull(points, qhull_options="Qc")
 # chull.points <n,3> coordinates
 # chull.vertices <k> indices of points that are vertices
 
-def printpoint(pi):
-    print(f"[{points[pi][0]:4.0f}, {points[pi][1]:4.0f}, {points[pi][2]:4.0f}] {descriptors[pi]}")
+# Process facets
+point_to_facet = {} # {pix: [f.., f..
+for ix, d in enumerate(chull.simplices): # [[p1, p2, p3], ...
+    for pix in d:
+        if pix not in point_to_facet: point_to_facet[pix] = []
+        point_to_facet[pix].append(f"f{ix}")
 
-print("VERTICES OF THE CONVEX HULL")
+# Process coplanar data
+coplanar = {}
+for d in chull.coplanar:
+    coplanar[d[0]] = f"cp(f{d[1]} #{d[2]})"
+
+def printpoint(pi):
+    print(f"// #{pi:4.0f} [{points[pi][0]:4.0f}, {points[pi][1]:4.0f}, {points[pi][2]:4.0f}] {descriptors[pi]} {'NC' if pi in nc_points else ''} {' '.join(point_to_facet.get(pi, []))} {coplanar.get(pi,'')}")
+
+print("// VERTICES OF THE CONVEX HULL")
 seen = set()
 for pi in chull.vertices:
     p = chull.points[pi]
@@ -72,16 +87,36 @@ for pi in chull.vertices:
     printpoint(pi)
     seen.add(pi)
 
-print("POINTS THAT ARE NOT VERTICES")
+print("// POINTS THAT ARE NOT VERTICES")
 for pi in range(len(points)):
     if pi not in seen:
         printpoint(pi)
         
-
 
 ## OpenSCAD output
 #hull = Collection(small_spheres).hull()
 #print(Header("best").render())
 #print(hull.render())
 #print(Collection(big_spheres).render())
-                
+
+if False:
+    # Convert the Qhull hull into openscad
+    point_reindex = {} # {old_index: new_index (only vertices)
+    for pinew, piold in enumerate(chull.vertices):
+        point_reindex[piold] = pinew
+    new_faces = [
+        [point_reindex[pi] for pi in face]
+        for face in chull.simplices
+    ]
+    ohull = Polyhedron(points=[points[pi] for pi in chull.vertices], faces=new_faces)
+    print(Header("best").render())
+    print(ohull.render())
+    
+    # Add points that are not vertices but NC (and perhaps coplanar)
+    for pi in range(len(points)):
+        if pi not in seen:
+            if pi in nc_points:
+                c = [0,.2,1]
+                if pi in coplanar: c=[1,0,0]
+                print(Sphere(big_r).move(points[pi]).color(*c).render())
+            
