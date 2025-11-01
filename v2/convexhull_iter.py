fc16eb473f1468cb701c28ad551e07042c0912c6
Elod Csirmaz <code@epcsirmaz.co.uk>
2025-11-01 18:11:50 +0000

    List edges

--- v2/convexhull_iter.py
+++ v2/convexhull_iter.py
@@ -11,7 +11,7 @@ import numpy as np
 
 sys.path.append(os.path.dirname(__file__) + "/../../openscad-py")  # https://codeberg.org/csirmaz/openscad-py
 
-from openscad_py import Sphere, Collection, Header, Polyhedron
+from openscad_py import Sphere, Collection, Header, Polyhedron, Point
 
 CREATE_SCAD = False
 
@@ -35,7 +35,9 @@ current_iteration = None
 points = []  # [[x,y,z], ...
 nc_points = {}  # {<point index>: <description str>, ...
 extra_points = {}  # {<point index>: <org point index>
-iteration_of_points = {}
+iteration_of_points = {}  # {<point index>: <iteration number>
+
+faces_of_prev_iter = None
 
 def printpoint(pi, p, is_vertex):
     if pi in extra_points:
@@ -45,15 +47,82 @@ def printpoint(pi, p, is_vertex):
             status = 'vertex'
         else:
             if iteration_of_points[pi] < current_iteration:
+                # Check that we are only dropping vertices from the previous iteration
+                assert current_iteration == iteration_of_points[pi] + 1
                 status = 'dropped_vertex'
             else:
                 status = 'not_vertex'
-    print(f"// #{pi:4.0f} [{p[0]:4.0f}, {p[1]:4.0f}, {p[2]:4.0f}] <{status}> {f'NC {nc_points[pi]}' if pi in nc_points else ''}")
+    print(f"// #{pi:4.0f} [{p[0]:4.0f}, {p[1]:4.0f}, {p[2]:4.0f}] {f'NC {nc_points[pi]}' if pi in nc_points else ''} <{status}>")
     # {' '.join(point_to_facet.get(pi, []))} {coplanar.get(pi,'')}
 
 
+def process_faces(chull):
+    # Merge faces and find real edges
+    # chull - hull of the current iteration
+    #    This tells us which vertex is still a vertex
+    # faces_of_prev_iter - faces from the previous iteration with the new point indices
+    #    Excludes faces that contained "extra" points
+    
+    if faces_of_prev_iter is None:
+        return
+    
+    point_to_face = {}  # {<point index>: set(<face index>, ...), ...
+    for fi, face in enumerate(faces_of_prev_iter):
+        for pi in face:
+            if pi not in point_to_face: point_to_face[pi] = set()
+            point_to_face[pi].add(fi)
+    
+    face_normals = []  # [Point(), ...
+    for face in faces_of_prev_iter:
+        p1 = Point(chull.points[face[0]])
+        p2 = Point(chull.points[face[1]])
+        p3 = Point(chull.points[face[2]])
+        norm = (p2-p1).cross(p3-p2).norm()
+        face_normals.append(norm)
+
+    seen_edges = set()
+    
+    def check_edge(pi1, pi2):
+        key1 = f"{pi1}:{pi2}"
+        if key1 in seen_edges: return None
+    
+        seen_edges.add(key1)
+        seen_edges.add(f"{pi2}:{pi1}")
+        
+        r = point_to_face[pi1].intersection(point_to_face[pi2])
+        assert len(r) > 0
+        if len(r) == 1: return 'edge_of_area'
+        assert len(r) == 2
+        f1 = r.pop()
+        f2 = r.pop()
+        
+        if face_normals[f1].allclose(face_normals[f2]):
+            return 'non_edge'
+        
+        return 'real_edge'
+
+    for fi, face in enumerate(faces_of_prev_iter):
+        for pointpair in [
+            [face[0], face[1]],
+            [face[1], face[2]],
+            [face[2], face[0]]
+        ]:
+            status = check_edge(*pointpair)
+            if status is None or status == 'non_edge': continue
+            is_vertex1 = pointpair[0] in chull.vertices
+            is_vertex2 = pointpair[1] in chull.vertices
+            if not is_vertex1: status += " (p1 will drop)"
+            if not is_vertex2: status += " (p2 will drop)"
+            p1 = chull.points[pointpair[0]]
+            p2 = chull.points[pointpair[1]]
+            print(f"// EDGE({p1[0]:.0f},{p1[1]:.0f},{p1[2]:.0f})-({p2[0]:.0f},{p2[1]:.0f},{p2[2]:.0f}): {status} (iteration={current_iteration-1})")
+            printpoint(pointpair[0], p1, is_vertex1)
+            printpoint(pointpair[1], p2, is_vertex2)
+            print("")
+            
+
 def process_points():
-    global max_y, max_z, current_iteration, points, nc_points, extra_points, iteration_of_points
+    global max_y, max_z, current_iteration, points, nc_points, extra_points, iteration_of_points, faces_of_prev_iter
     
     # Add extra points to create triangles
     assert max_y == max_z
@@ -67,9 +136,15 @@ def process_points():
     points = np.array(points)
     
     chull = ConvexHull(points, qhull_options="Qc")
-    # chull.points <n,3> coordinates
+    # chull.points shape=<n,3> coordinates of all points
+    # chull.vertices shape=<k> indices of points that are vertices
+    # chull.simplices  triads of indices of points that form faces (triangulated)
     # We have previously checked that chull.points === points (indices match)
     points = None
+    
+    # Process faces from the previous iteration
+    # We have already reindexed the points to match this iteration
+    process_faces(chull)
     
     # Create report
     
@@ -95,19 +170,36 @@ def process_points():
                 
     print(f"// REPORT END iteration={current_iteration}", flush=True)
     
+    print(f"// ! Reindexing points...")
+    
     # Initialize new input from the vertices of this hull to save space/time
-    points = []
-    nc_points_new = {}
-    iteration_of_points_new = {}
+    points = []  # global
+    nc_points_new = {}  # local
+    iteration_of_points_new = {}  # local
+    point_reindex = {}  # local, {<old point index>: <new point index>, ...
     for pi in chull.vertices:
-        if not pi in extra_points:
+        if pi not in extra_points:
             p = chull.points[pi]
             pi_new = len(points)
             # print(f"//  Mapping #{pi} -> #{pi_new}")
+            point_reindex[pi] = pi_new
             points.append(p)
             if pi in nc_points: nc_points_new[pi_new] = nc_points[pi]
             if pi in iteration_of_points: iteration_of_points_new[pi_new] = iteration_of_points[pi]
         
+    # Store faces for processing later
+    # We do it in the next step as that's when we know which vertices stay
+    faces_of_prev_iter = []
+    for face in chull.simplices:
+        face = [int(v) for v in face]
+        if face[0] not in extra_points and face[1] not in extra_points and face[2] not in extra_points:
+            faces_of_prev_iter.append([
+                point_reindex[face[0]],
+                point_reindex[face[1]],
+                point_reindex[face[2]]
+            ])
+    
+    # Set global data structures
     nc_points = nc_points_new; del nc_points_new
     iteration_of_points = iteration_of_points_new; del iteration_of_points_new
     extra_points = {}
@@ -134,13 +226,12 @@ for line in sys.stdin:
         if max_z < z: max_z = z
         if check_necessary_condition(desc):
             # save the descriptor string but only if the necessary condition is met
-            nc_points[pi_new] = f"({itr:2.0f}) <{desc}>"
+            nc_points[pi_new] = f"<{desc}> (iter={itr})"
             iteration_of_points[pi_new] = itr
 
 process_points()
 
 
-# chull.vertices <k> indices of points that are vertices
 
 ## Process facets
 #point_to_facet = {} # {pix: [f.., f..
